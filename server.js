66022e5c2efc95caeb7ee41b9e491e1271d69a1b
Uttkarsh <uttkarshpahuja@gmail.com>
2026-07-27 19:26:44 +0530

    add timeouts

--- server.js
+++ server.js
@@ -234,6 +234,9 @@ app.post('/send-message', async (req, res) => {
       host: process.env.EMAIL_HOST,
       port: Number(process.env.EMAIL_PORT),
       secure: process.env.EMAIL_SECURE === true,
+      connectionTimeout: 30000,
+      greetingTimeout: 30000,
+      socketTimeout: 30000,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
