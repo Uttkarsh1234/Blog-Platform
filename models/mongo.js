1bf555a36a4f1db227953c040141124cd2050005
Uttkarsh <uttkarshpahuja@gmail.com>
2026-07-28 16:18:19 +0530

    redo changes of path password

--- models/mongo.js
+++ models/mongo.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 
 const UserSchema = mongoose.Schema({
-    name: {type: String, required: true},
-    email: {type: String,required: true},
-    password: {type: String,required: true}
+    name: {type: String},
+    email: {type: String},
+    password: {type: String}
 })
 
 module.exports = mongoose.model("Blog",UserSchema);
